c07e77610b3b3faffbe8819915079d20322b60ff
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-09-20 14:12:29 +0000

    realloc

--- realloc.c
+++ realloc.c
@@ -7,10 +7,11 @@
  * @new_size: new size
  * Return: pointer
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *pptr = NULL, *rel = NULL;
-	unsigned int i = 0;
+	char *pptr, *rel;
+	unsigned int i;
 
 	if (ptr != NULL)
 		pptr = ptr;
@@ -20,19 +21,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (NULL);
+		return (0);
 	}
+
 	rel = malloc(new_size);
 	if (rel == NULL)
-		return (NULL);
+		return (0);
 
 	for (i = 0; i < old_size && i < new_size; i++)
 	{
 		rel[i] = pptr[i];
 	}
+
 	free(ptr);
 	return (rel);
 }
