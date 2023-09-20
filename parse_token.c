089c5d5b0644f17c4737017e5d7632cdc9e383ed
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-09-20 20:27:55 +0000

    done refactoring parse_token

--- parse_token.c
+++ parse_token.c
@@ -1,5 +1,24 @@
 #include "main.h"
+/**
+ * free_tokens - frees the array of tokens
+ * @tokens: the array of strings to free
+ * @argc: the number of token
+ * Return: nothing
+ */
+void free_tokens(char **tokens, int argc)
+{
+	int i = 0;
 
+	perror("free tokens func: freeing failed");
+	/*free allocs*/
+	for (i = 0; i < argc; i++)
+	{
+		if (tokens[i] != NULL)/*updated*/
+			free(tokens[i]);
+	}
+	free(tokens);
+	exit(EXIT_FAILURE);/** NOTES we may need return values*/
+}
 /**
  * parse_tokens - breaks inputs into array of tokens/words
  * @input: arguments from user
@@ -8,14 +27,11 @@
  */
 char **parse_tokens(char *input, int *argc)
 {
-	/*Parse the inputs into tokens and store in an array*/
 	char **tokens = NULL;/*the eventual array to return*/
 	char *split = NULL;/*keep track of each pieces*/
-	int i = 0;
-	/*handle empty input */
-	if (input == NULL || input[0] == '\0' || input[0] == '\n'|| input[0] == ' ')
+	/* if user passes just a new line*/
+	if (input == NULL || input[0] == '\0' || input[0] == '\n' || input[0] == ' ')
 	{
-		/*handle empty input*/
 		tokens = (char **)malloc(sizeof(char *) * 2);
 		if (tokens == NULL)
 		{
@@ -26,46 +42,20 @@ char **parse_tokens(char *input, int *argc)
 		*argc = 0;
 		return (tokens);
 	}
-	/*call strtok to split input based on delimiters*/
-	split = strtok(input, " \t\n");
+	split = strtok(input, " \t\n");/*call strtok to split input based on delim*/
 	*argc = 0;/*keep track of number of token 4 space alloc*/
-	/*get other fragments and store inside the tokens array*/
-	while (split != NULL)
+	while (split != NULL)/*get fragments and store inside the tokens array*/
 	{
-		/*custom realloc,prev size,&the new size,2bytes 4 NULL.T/NULL*/
 		tokens = (char **)_realloc(tokens, ((*argc) * sizeof(char *)),
-				((*argc) + 2) * sizeof(char *));
-		/*HANDLE REALLOC RETURN*/
-		if (tokens == NULL)
-		{
-			perror("Token allocation failed");
-			/*free allocs*/
-			for (i = 0; i < *argc; i++)
-			{
-				if(tokens[i] != NULL)/*updated*/
-					free(tokens[i]);
-			}
-			free(tokens);
-			exit(EXIT_FAILURE);/** NOTES we may need return values*/
-		}
-		/*STORE TOKENS INTO ARRAY BASED ON ARGC*/
-		tokens[*argc] = _strdup(split);
+				((*argc) + 2) * sizeof(char *));/*c_realloc,prev,new,2bytes-NUL.T/NUL*/
+		if (tokens == NULL)/*HANDLE REALLOC RETURN*/
+			free_tokens(tokens, *argc);
+		tokens[*argc] = _strdup(split);/*STORE TOKENS INTO ARRAY BASED ON ARGC*/
 		if (tokens[*argc] == NULL)
-		{
-			perror("alloc failed: tokens[*argc]");
-			for (i = 0; i < *argc; i++)
-			{
-				if(tokens[i] != NULL)
-					free(tokens[i]);
-			}
-			free(tokens);
-			exit(EXIT_FAILURE);
-		}
+			free_tokens(tokens, *argc);
 		(*argc)++;
-		/*get other fragments*/
-		split = strtok(NULL, " \t\n");
+		split = strtok(NULL, " \t\n");/*get other fragments*/
 	}
-	/*set last value to NULL*/
-	tokens[*argc] = NULL;
+	tokens[*argc] = NULL;/*set last value to NULL*/
 	return (tokens);/*must be freed manually by user*/
 }
